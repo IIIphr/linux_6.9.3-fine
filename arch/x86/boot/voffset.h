2a79617dc3a57990d9cb1017d248a9bc136fb4a1
IIIphr <sepehr.ebi.1380@gmail.com>
2025-02-08 00:26:51 +0330

    First make!

---
+++ arch/x86/boot/voffset.h
@@ -0,0 +1,4 @@
+#define VO___bss_start _AC(0xffffffff83b63000,UL)
+#define VO___start_rodata _AC(0xffffffff82400000,UL)
+#define VO__end _AC(0xffffffff84030000,UL)
+#define VO__text _AC(0xffffffff81000000,UL)
