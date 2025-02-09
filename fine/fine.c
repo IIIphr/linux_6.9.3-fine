fff855f30889ce14010333b7df85cddd2e7b5faa
IIIphr <sepehr.ebi.1380@gmail.com>
2025-02-09 12:37:26 +0330

    Trying the jmp instruction

--- fine/fine.c
+++ fine/fine.c
@@ -3,5 +3,9 @@
 
 SYSCALL_DEFINE0(fine) {
 	printk("==== FINE systemcall ====\n");
+	printk("====      START      ====\n");
+	asm volatile("jmp 0x01");
+	printk("====      JUMPED     ====\n");
+	printk("====       END       ====\n");
 	return 0;
 }
